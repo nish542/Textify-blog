6b374eb4ca67ca458d50af13e58172a512d4d248
Nishant Anand <nishant.anand542@gmail.com>
2025-05-17 22:55:55 +0530

    latest commit

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Configure logging
+
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
