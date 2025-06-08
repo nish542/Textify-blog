15af6617c6440a4ac692a5e688759c2c48060be7
Nishant Anand <nishant.anand542@gmail.com>
2025-06-08 19:14:55 +0530

    Aligned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -142,7 +142,7 @@ function App() {
 
       {/* Main content with padding for fixed navbar */}
       <div style={{ 
-        paddingTop: '120px', // Adjust this value based on your navbar height
+        paddingTop: '60px', // Adjust this value based on your navbar height
         position: 'relative',
         zIndex: 1
       }}>
